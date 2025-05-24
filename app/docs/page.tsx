cb8ddef76b7281fe16af2e8e71f5d716bbbbf0f6
Apurba Sundar Nayak <apurbasundar2002@gmail.com>
2025-05-24 12:21:16 +0530

    done some changes to show docs

---
+++ app/docs/page.tsx
@@ -0,0 +1,8 @@
+export default function SwaggerDocs() {
+  return (
+    <iframe
+      src="http://135.13.37.72:8080/durl-docs"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+    />
+  );
+}
